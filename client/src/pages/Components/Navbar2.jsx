2feb5db05b524d00de85df540054479036c617fd
devynj4722 <jmoneyaae@gmail.com>
2024-03-26 00:07:43 -0700

    Update Navbar2.jsx

--- client/src/pages/Components/Navbar2.jsx
+++ client/src/pages/Components/Navbar2.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Navbar2.scss'
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom/dist/umd/react-router-dom.development';
 function NavBar2() {
 
 return (
@@ -8,8 +9,15 @@ return (
 <>
 <Nav defaultActiveKey="/" className="flex-column">
     
-      <Nav.Link href='/shop' eventKey="/shop">Shop</Nav.Link>
-      <Nav.Link href='/contact' eventKey="link-2">Contact</Nav.Link>
+    <Link to='/shop'>
+    
+    <Nav.Link  eventKey="/shop">Shop</Nav.Link>
+    </Link>  
+    <Link to='/contact'>
+    
+    
+      <Nav.Link  eventKey="link-2">Contact</Nav.Link>
+    </Link>
   
        
     </Nav>
